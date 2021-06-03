36cc1d96e496dc2b3078637cddd1d43e28a6dea1
Pierre Tardy <pierre.tardy@renault.com>
2021-06-03 11:33:29 +0200

    don't crash if ssh is not present in the system, and the manifest is not using ssh urls

--- repo/ssh.py
+++ repo/ssh.py
@@ -53,8 +53,9 @@ def version():
   try:
     return _parse_ssh_version()
   except subprocess.CalledProcessError:
-    print('fatal: unable to detect ssh version', file=sys.stderr)
-    sys.exit(1)
+    print('warning: unable to detect ssh version', file=sys.stderr)
+    # don't crash if ssh is not present in the system, and the manifest is not using ssh urls
+    return (8,2)  # assume recent version
 
 
 URI_SCP = re.compile(r'^([^@:]*@?[^:/]{1,}):')
